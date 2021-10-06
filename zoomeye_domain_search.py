3c61067e19fcc859caf1342d15c4c85d91a61b40
鬼人阿金 <82949124+m4r7k1@users.noreply.github.com>
2021-10-06 21:35:10 +0800

    Update zoomeye_domain_search.py

--- zoomeye_domain_search.py
+++ zoomeye_domain_search.py
@@ -5,21 +5,21 @@ def zoomeye_domain_search(domian):
     headers = {
         'API-KEY': '你的API-KEY'
     }
-    page = 1  # 初始化page值为1
-    domain_list = []  # 用于存储每页的子域名
-    ip_list = []  # 用于存储每页子域名的IP地址
+    page = 1  
+    domain_list = []  
+    ip_list = [] 
     while True:
         url = "https://api.zoomeye.org/domain/search?q=" + domian + "&page=" + str(page) + "&type=1"
-        response = requests.get(url, headers=headers)  # 发送get请求，附带自定义http头
+        response = requests.get(url, headers=headers)  
         result = response.json()
         if result['list'] == []:
             break
 
-        for i in result['list']:  # 提取list中的数据
-            domain_list.append(i['name'])  # 提取子域名
-            ip_list.append(i['ip'])  # 提取ip
+        for i in result['list']:  
+            domain_list.append(i['name'])  
+            ip_list.append(i['ip']) 
 
-        page = page + 1  # 控制页码
+        page = page + 1  
 
     return domain_list, ip_list
 
@@ -28,5 +28,5 @@ if __name__ == "__main__":
     domain = input("请输入查询的域名：")
     domain_list, ip_list = zoomeye_domain_search(domain)
 
-for i in range(len(domain_list)):  # 输出结果
+for i in range(len(domain_list)): 
     print(domain_list[i], ip_list[i])
